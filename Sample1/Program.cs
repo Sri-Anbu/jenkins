b91088a8fbe474c89ba71f0064bfd492ec85de8d
Sri-Anbu <76032790+Sri-Anbu@users.noreply.github.com>
2020-12-17 13:07:41 +0530

    Update Program.cs

--- Sample1/Program.cs
+++ Sample1/Program.cs
@@ -11,7 +11,7 @@ namespace Sample1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Conole.WeiteLine("Added after firstbuild");
+            Conole.WeiteLine("Added after firstbuild")
         }
     }
 }
